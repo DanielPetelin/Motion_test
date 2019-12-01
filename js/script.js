f3027ed6283a702289f9f7feead93dfb680daaac
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-01 10:02:46 +0300

    ++

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ var left = 0;
 document.onkeydown = function (event) {
     console.log(event);
     if (event.key == 'ArrowRight') {
-        block.style.left = left + 'px'
+        block.style.left = left + 'px';
+        left++;
     }
 }
